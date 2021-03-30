526b6ee5433c2fce2603d0847449934176115181
wends155 <saliganw@gmail.com>
2021-03-30 17:22:47 +0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Tailwindcss from './Tailwindcss.svelte';
+	
+	
+</script>
+
+<Tailwindcss />
+<main>
+	
+</main>
+
+<style lang="postcss">
+	
+</style>
---
+++ src/Card.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<style>
+
+</style>
+
+<main>
+    <div>Card</div>
+
+</main>
